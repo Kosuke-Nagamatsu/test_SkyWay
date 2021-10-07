0aff38ab70cbe67dc945abcae0d9a950e6686456
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-10-08 01:31:09 +0900

    Add comments

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,7 +1,8 @@
 window.onload = () => {
   
   const Peer = window.Peer;
-  const SKYWEY_KEY = gon.api_key;
+  // SkyWayのAPIキーを定数に保存
+  const SKYWAY_KEY = gon.api_key;
   
   (async function main() {
     const localVideo = document.getElementById('js-local-stream');
@@ -18,6 +19,7 @@ window.onload = () => {
       SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
     `.trim();
     
+    // getUserMediaで取得した自分のカメラ映像をlocalStreamへ保存
     const localStream = await navigator.mediaDevices
       .getUserMedia({
         audio: true,
@@ -30,20 +32,28 @@ window.onload = () => {
     localVideo.srcObject = localStream;
     localVideo.playsInline = true;
     await localVideo.play().catch(console.error);
-      
+
+    // Peer作成
+    // new Peer() により、SkyWay のシグナリングサーバと接続できる
     const peer = (window.peer = new Peer({
-      key: SKYWEY_KEY,
+      key: SKYWAY_KEY,
       debug: 3,
     }));
-      
+
+    // 発信者側
+    // Callボタンに対して、clickイベントが発生したときに呼び出される関数を設定
     // Register caller handler
     callTrigger.addEventListener('click', () => {
       // Note that you need to ensure the peer has connected to signaling server
       // before using methods of peer instance.
       if (!peer.open) {
+        // peer.open は、シグナリングサーバへの接続状況をboolean型で保持してる
         return;
       }
       
+      // call(相手のPeerID, localStream)
+      // ↑これで相手に接続し、MediaConnectionオブジェクトが取得できる
+      // MediaConnectionには相手の映像が含まれる
       const mediaConnection = peer.call(remoteId.value, localStream);
       
       mediaConnection.on('stream', async stream => {
@@ -58,17 +68,22 @@ window.onload = () => {
         remoteVideo.srcObject = null;
       });
       
+      // 切断処理
+      // close(true)で相手の MediaConnection も即座に close する
       closeTrigger.addEventListener('click', () => mediaConnection.close(true));
     });
     
+    // シグナリングサーバとの接続が成功したタイミングでview側へPeerIDを挿入
     peer.once('open', id => (localId.textContent = id));
     
-    // Register callee handler
+    // 受信者側
+    // 発信側からメディアチャネル（音声・映像）の接続を受信したときにlocalStreamを送る
+    // Register caller handler
     peer.on('call', mediaConnection => {
       mediaConnection.answer(localStream);
       
       mediaConnection.on('stream', async stream => {
-        // Render remote stream for callee
+        // Render remote stream for caller
         remoteVideo.srcObject = stream;
         remoteVideo.playsInline = true;
         await remoteVideo.play().catch(console.error);
@@ -79,6 +94,8 @@ window.onload = () => {
         remoteVideo.srcObject = null;
       });
       
+      // 切断処理
+      // close(true)で相手の MediaConnection も即座に close する
       closeTrigger.addEventListener('click', () => mediaConnection.close(true));
     });
     
