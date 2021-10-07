43f88b41b2d44989f79c285289e601f411a33b07
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-10-07 19:46:13 +0900

    Add JS file from SkyWay and Update; Update chat.html.erb as sample code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// 追加
+import 'script'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
