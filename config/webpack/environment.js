090b356da0896f61926621b95cf72b547045d650
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-10-12 10:31:00 +0900

    Re-install webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    Popper: ['popper.js', 'default']
-  })
-)
-
 module.exports = environment
