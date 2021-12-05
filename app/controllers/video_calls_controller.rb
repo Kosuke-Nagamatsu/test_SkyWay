f130b2907799b718b5b7de4d25fcd3e7097504f2
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-12-05 12:16:15 +0900

    add : processes on 'video_calls' controller for js file and video chat page

--- app/controllers/video_calls_controller.rb
+++ app/controllers/video_calls_controller.rb
@@ -1,4 +1,14 @@
 class VideoCallsController < ApplicationController
   def chat
+    @users =
+      if user_signed_in?
+        User.where.not(id: current_user.id)
+      else
+        User.all
+      end
+
+    # ログイン済みの場合、peer_id を
+    # その他は false をvideo_calls/index.jsへ渡す
+    gon.user_peer_id = user_signed_in? ? current_user.peer_id : false
   end
 end
