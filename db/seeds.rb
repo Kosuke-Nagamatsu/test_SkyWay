4f66d6862cb50f25962bbd8347a1bf5519a722c9
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-12-06 09:11:13 +0900

    update : remove unused folder, align seed indentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 3.times do |n|
-    User.create!(
-      name: "ユーザ#{n + 1}",
-      email: "user#{n + 1}@example.com",
-      password: 'password',
-      peer_id: [*'A'..'Z', *'a'..'z', *'0'..'9'].sample(16).join
-    )
-  end
+  User.create!(
+    name: "ユーザ#{n + 1}",
+    email: "user#{n + 1}@example.com",
+    password: 'password',
+    peer_id: [*'A'..'Z', *'a'..'z', *'0'..'9'].sample(16).join
+  )
+end
